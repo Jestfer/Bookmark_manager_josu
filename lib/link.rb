9a87347f038509a1e95b8b015946911b9993a01c
Josué Estévez Fernández <jestfer6@gmail.com>
2018-02-12 20:22:28 +0000

    added Data Mapper description by Eddie into link.rb

--- lib/link.rb
+++ lib/link.rb
@@ -2,6 +2,9 @@ require 'pg'
 
 class Link
   def self.all
+    # Data mapper will enable us to stop establishing
+    # connections - We get the methods 'for free'
+    # Creates instance and saves it into the DB
     connection = PG.connect(dbname: 'bookmark_manager')
     result = connection.exec("SELECT * FROM links")
     result.map { |hash| hash['url'] }
